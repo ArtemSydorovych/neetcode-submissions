05b49157afb146bc94e4ba5c40fb6beeb549b907
ArtemSydorovych <68222015+ArtemSydorovych@users.noreply.github.com>
2026-09-14 09:42:12 +0200

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.py
@@ -0,0 +1,13 @@
+from functools import cache
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        stoneSum = sum(stones)
+        target = stoneSum + 1 // 2
+        
+        @cache
+        def dfs(i, total):
+            if total > target or i == len(stones):
+                return abs(total - (stoneSum - total))
+            return min(dfs(i + 1, total), dfs(i+1, total + stones[i]))
+    
+        return dfs(0,0)
